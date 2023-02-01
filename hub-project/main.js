fed56157de5477649dc5f33dc28046e6b4170042
PabloTei <pteijeirolopez@gmail.com>
2023-02-01 16:17:04 +0100

    iteration2

--- hub-project/main.js
+++ hub-project/main.js
@@ -1,8 +1,10 @@
 import './style.css';
 import { Header } from './components/Header/Header';
 import { changeColor } from './components/Button/Button';
+import { Login } from './pages/Login/Login';
 
 
 const header = document.querySelector("header");
 header.innerHTML = Header();
 changeColor();
+Login();
---
+++ hub-project/pages/Login/Login.js
@@ -0,0 +1,13 @@
+import "./Login.css";
+
+export const Login = () => {
+    const main = document.querySelector("main");
+    main.innerHTML = `
+    <section class="login">
+    <h1>NEOLAND</h1>
+    <h1>HUB-GAMES</h1>
+    <input>
+    <button></button>
+    </section>
+    `
+}
