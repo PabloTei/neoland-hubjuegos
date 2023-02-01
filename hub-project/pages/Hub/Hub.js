1c22e99155c0801a44bf794099cdda886c17a513
PabloTei <pteijeirolopez@gmail.com>
2023-02-01 19:53:01 +0100

    iteration6

--- hub-project/pages/Hub/Hub.js
+++ hub-project/pages/Hub/Hub.js
@@ -1,9 +1,11 @@
 import "./Hub.css";
 import { initContent } from "../../main";
+import { Login } from "../Login/Login";
 
 export const Hub = () => {
     const main = document.querySelector("main");
     main.innerHTML = `
+    <h1>Hola ${localStorage.getItem("user")}</h1>
     <button class="button-logout">Logout</button>
     `
     logout();
@@ -12,6 +14,7 @@ export const Hub = () => {
 const logout = () => {
     const inputLogout = document.querySelector(".button-logout");
     inputLogout.addEventListener("click", (ev) => {
-        initContent("Login")
+        localStorage.removeItem("user");
+        initContent("Login");
     })
 }
