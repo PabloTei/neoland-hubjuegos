7496b4dbe20f77215b9337fb73ca3b5088a74edb
PabloTei <pteijeirolopez@gmail.com>
2023-02-01 22:59:36 +0100

    iteration

---
+++ hub-project/components/ProjectGame/ProjectGame.js
@@ -0,0 +1,5 @@
+import "./ProjectGame.css";
+
+export const ProjectGame = (game) => `
+    <h2 class="${game}-name">${game}</h2>
+`
--- hub-project/pages/Hub/Hub.js
+++ hub-project/pages/Hub/Hub.js
@@ -1,13 +1,20 @@
 import "./Hub.css";
 import { initContent } from "../../main";
+import { ProjectGame } from "../../components/ProjectGame/ProjectGame";
+import { games } from "../../data/games";
 
 export const Hub = () => {
     const main = document.querySelector("main");
     main.innerHTML = `
+    <section class="games">
     <h1>Bienvenido ${localStorage.getItem("user")}</h1>
+    <p>Escoge un juego</p>
+    <div class="game-container"></div>
     <button class="button-logout">Logout</button>
+    </section>
     `
     logout();
+    hubGames();
 }
 
 const logout = () => {
@@ -16,4 +23,13 @@ const logout = () => {
         localStorage.removeItem("user");
         initContent("Login");
     })
+}
+
+const hubGames = () => {
+        const container = document.querySelector(".game-container");
+        for (const game of games) {
+            const div = document.createElement("div");
+            div.innerHTML = ProjectGame(game);
+            container.appendChild(div);
+        }
 }
