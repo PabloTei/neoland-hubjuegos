dcded77b4a1f46a2d04719f3cc7361186aa4c8dc
PabloTei <pteijeirolopez@gmail.com>
2023-02-02 16:19:08 +0100

    get pokeapi

--- hub-project/pages/PokeApi/PokeApi.js
+++ hub-project/pages/PokeApi/PokeApi.js
@@ -1,11 +1,25 @@
+import axios from "axios";
 import "./PokeApi.css";
+const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/"
 
+const myPokemons = () => {
+    let allPokemons = [];
+    for (let i = 1; i < 151; i++) {
+        axios.get(`${POKEMON_URL}${i}`).then((res) => {
+            allPokemons = res.data;
+            console.log(allPokemons);
+        })
+    }
+}
 
 export const PokeApi = () => {
     const main = document.querySelector("main");
     main.innerHTML = 
     `
     <h1>hola</h1>
+
     `
+    myPokemons();
 }
+
 
