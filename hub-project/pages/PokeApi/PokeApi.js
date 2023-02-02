c6e281c9b214b96e01e5e8eded4280ff5bf5cf4a
PabloTei <pteijeirolopez@gmail.com>
2023-02-02 19:28:18 +0100

    iteration

--- hub-project/pages/PokeApi/PokeApi.js
+++ hub-project/pages/PokeApi/PokeApi.js
@@ -1,23 +1,25 @@
-import axios from "axios";
 import "./PokeApi.css";
 const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/"
 
-const myPokemons = () => {
+const myPokemons = async () => {
     let allPokemons = [];
     for (let i = 1; i < 151; i++) {
-        axios.get(`${POKEMON_URL}${i}`).then((res) => {
-            allPokemons = res.data;
-            console.log(allPokemons);
-        })
+        const data = await fetch (`${POKEMON_URL}${i}`)
+        const info = await data.json();
+        allPokemons.push(info);
+    }
+
+    for (const pokemon of allPokemons) {
+        console.log(pokemon.name)
     }
 }
+
 
 export const PokeApi = () => {
     const main = document.querySelector("main");
     main.innerHTML = 
     `
     <h1>hola</h1>
-
     `
     myPokemons();
 }
