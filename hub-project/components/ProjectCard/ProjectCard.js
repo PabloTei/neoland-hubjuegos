6d6fc4e94595212242462b3b05b58d35b6e2111a
PabloTei <pteijeirolopez@gmail.com>
2023-02-03 15:53:34 +0100

    añadir datos pokeapi

--- hub-project/components/ProjectCard/ProjectCard.js
+++ hub-project/components/ProjectCard/ProjectCard.js
@@ -3,5 +3,6 @@ import "./ProjectCard.css";
 export const ProjectCard = (pokemon) => `
     <img src="${pokemon.sprites.other.dream_world.front_default}">
     <h2>${(pokemon.name).toUpperCase()}</h2>
+    <p>Altura: ${pokemon.height} | Peso: ${pokemon.weight}</p>
 `
 
