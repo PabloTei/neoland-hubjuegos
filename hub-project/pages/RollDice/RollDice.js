13c49e031e0137649fa8aed6c22774d1181e3a58
PabloTei <pteijeirolopez@gmail.com>
2023-02-05 00:12:47 +0100

    roll dice codigo finalizado

--- hub-project/pages/RollDice/RollDice.js
+++ hub-project/pages/RollDice/RollDice.js
@@ -13,10 +13,47 @@ export const RollDice = () => {
     <div class="dado-dos"></div>
     </div>
     <div class="boton-resultado">
-    <p>/resultado/</p>
+    <p class="resultado">/resultado/</p>
     <button class="roll">¡ROLL DICE!</button>
     </div>
     </div>
     `
+
+    btnRollDice();
 }
+
+const getRandomNumber = () => {
+    const jugador = Math.floor(Math.random() * (6 - 1) + 1);
+    const maquina = Math.floor(Math.random() * (6 - 1) + 1);
+    const dadoUser = document.querySelector(".jugador-uno");
+    const dadoMaquina = document.querySelector(".jugador-dos");
+    const resultado = document.querySelector(".resultado");
+    dadoUser.innerHTML = `
+    ${jugador}
+    `
+    dadoMaquina.innerHTML = `
+    ${maquina}
+    `
+    if (jugador > maquina) {
+        resultado.innerHTML = `
+        ${localStorage.getItem("user").toUpperCase()} ha ganado.
+        `
+    } else if (jugador < maquina) {
+        resultado.innerHTML = `
+        Máquina ha ganado.
+        `
+    } else {
+        resultado.innerHTML = `
+        Habéis empatado.
+        `
+    }
+}
+
+const btnRollDice = () => {
+    const buttonRollDice = document.querySelector(".roll");
+    buttonRollDice.addEventListener("click", () => {
+        getRandomNumber();
+    })
+}
+
 
