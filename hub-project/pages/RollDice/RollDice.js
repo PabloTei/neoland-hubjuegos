b981c9f519bfd0ee32a948ec2b6b0bdaf1157731
PabloTei <pteijeirolopez@gmail.com>
2023-02-04 23:36:10 +0100

    diseño rolldice

--- hub-project/pages/RollDice/RollDice.js
+++ hub-project/pages/RollDice/RollDice.js
@@ -1,9 +1,22 @@
-import { initContent } from "../../main";
 import "./RollDice.css";
 
 export const RollDice = () => {
     const main = document.querySelector("main");
     main.innerHTML = `
-    <h1>ESTAMOS EN ROLL DICE</h1>
+    <div class="general">
+    <div class="jugador-uno">
+    <h3>${localStorage.getItem("user").toUpperCase()}</h3>
+    <div class="dado-uno"></div>
+    </div>
+    <div class="jugador-dos">
+    <h3>MAQUINA</h3>
+    <div class="dado-dos"></div>
+    </div>
+    <div class="boton-resultado">
+    <p>/resultado/</p>
+    <button class="roll">¡ROLL DICE!</button>
+    </div>
+    </div>
     `
 }
+
