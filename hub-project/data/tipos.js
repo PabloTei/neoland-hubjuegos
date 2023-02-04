d442ef73153df687209a1ea2f15c361c6bc118fb
PabloTei <pteijeirolopez@gmail.com>
2023-02-04 13:37:57 +0100

    imagenes tipo añadidas

--- hub-project/data/tipos.js
+++ hub-project/data/tipos.js
@@ -22,93 +22,93 @@
 export const tiposPokemonObject = [
     {
         name: "bug", 
-        img: ""
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513419/samples/Tipo%20Pokemoon/bug_rj67mg.svg"
         
     }, 
 
     {
         name: "dark", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513433/samples/Tipo%20Pokemoon/dark_gpez6f.svg"    
     },
 
     {
         name: "dragon", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513440/samples/Tipo%20Pokemoon/dragon_eljonv.svg"    
     },
 
     {
         name: "electric", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513445/samples/Tipo%20Pokemoon/electric_pcxzfi.svg"    
     },
 
     {
         name: "fairy", 
-        img: ""
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513452/samples/Tipo%20Pokemoon/fairy_rgcq88.svg"
     },
 
     {
         name: "fighting", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513457/samples/Tipo%20Pokemoon/fighting_a9jzjl.svg"    
     },
 
     {
         name: "fire", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513470/samples/Tipo%20Pokemoon/fire_nzseei.svg"    
     },
 
     {
         name: "flying", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513476/samples/Tipo%20Pokemoon/flying_imi73q.svg"    
     },
 
     {
         name: "ghost", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513482/samples/Tipo%20Pokemoon/ghost_tqdf5p.svg"    
     },
 
     {
         name: "grass", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513487/samples/Tipo%20Pokemoon/grass_tvvn6m.svg"    
     },
 
     {
         name: "ground", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513502/samples/Tipo%20Pokemoon/ground_zvippd.svg"    
     },
 
     {
         name: "ice", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513507/samples/Tipo%20Pokemoon/ice_d3fewl.svg"    
     },
 
     {
         name: "normal", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513514/samples/Tipo%20Pokemoon/normal_xgaoqw.svg"    
     },
 
     {
         name: "poison", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513527/samples/Tipo%20Pokemoon/poison_ts8zb8.svg"    
     },
 
     {
         name: "psychic", 
-        img: ""   
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513538/samples/Tipo%20Pokemoon/psychic_huguwn.svg"   
     },
 
     {
         name: "rock", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513545/samples/Tipo%20Pokemoon/rock_kyjjcy.svg"    
     },
 
     {
         name: "steel", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513551/samples/Tipo%20Pokemoon/steel_w5lib6.svg"    
     },
 
     {
         name: "water", 
-        img: ""    
+        img: "https://res.cloudinary.com/depifliz3/image/upload/v1675513557/samples/Tipo%20Pokemoon/water_i5vgdr.svg"    
     }
 ]
  
